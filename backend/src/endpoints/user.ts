4455daa7db1a623ed80e5b888b0593631c171dc6
omzkiii <geogeomarsantos@gmail.com>
2025-11-18 06:53:32 +0800

    backend: add caching using redis

--- backend/src/endpoints/user.ts
+++ backend/src/endpoints/user.ts
@@ -1,27 +1,33 @@
 import { Router } from "express";
 import type { Request, Response } from "express";
-import { prisma } from "../index.js";
+import { prisma, redis } from "../index.js";
 
 export const user = Router();
 
 user.get("/users", async (req: Request, res: Response) => {
-  const data = await prisma.user.findMany({
-    omit: {
-      sourceId: true,
-      statusId: true,
-      vehicleOfInterestId: true,
-    },
-    include: {
-      source: { select: { name: true } },
-      status: { select: { name: true } },
-      vehicleOfInterest: { select: { name: true } },
-    },
-  });
-  const users = data.map((d) => ({
-    ...d,
-    source: d.source.name,
-    status: d.status.name,
-    vehicleOfInterest: d.vehicleOfInterest.name,
-  }));
-  res.send(users);
+  const cached = await redis.get("users");
+  if (cached) {
+    res.send(JSON.parse(cached));
+  } else {
+    const data = await prisma.user.findMany({
+      omit: {
+        sourceId: true,
+        statusId: true,
+        vehicleOfInterestId: true,
+      },
+      include: {
+        source: { select: { name: true } },
+        status: { select: { name: true } },
+        vehicleOfInterest: { select: { name: true } },
+      },
+    });
+    const users = data.map((d) => ({
+      ...d,
+      source: d.source.name,
+      status: d.status.name,
+      vehicleOfInterest: d.vehicleOfInterest.name,
+    }));
+    await redis.set("users", JSON.stringify(users), { EX: 60 });
+    res.send(users);
+  }
 });
