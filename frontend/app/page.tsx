546d2488faea9fe6e3287492d8f6de48c595f795
omzkiii <geogeomarsantos@gmail.com>
2025-11-19 09:07:23 +0800

    frontend: add homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,7 +1,14 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-black">
+      <Link
+        href="/admin"
+        className="px-16 py-10 text-4xl font-bold rounded-2xl bg-blue-600 text-white hover:bg-blue-700 transition"
+      >
+        Go to Admin
+      </Link>
+    </div>
   );
 }
